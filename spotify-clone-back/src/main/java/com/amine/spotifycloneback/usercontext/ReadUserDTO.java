74f4bbebb509d06007d4fa6c857d8714a5cddd9d
Amine-elattllati <126120495+Amine-elattllati@users.noreply.github.com>
2024-05-23 19:21:05 +0100

    Create ReadUserDTO

---
+++ spotify-clone-back/src/main/java/com/amine/spotifycloneback/usercontext/ReadUserDTO.java
@@ -0,0 +1,5 @@
+package com.amine.spotifycloneback.usercontext;
+
+public record ReadUserDTO(String firstName, String lastName, String email, String imageUrl) {
+
+}
--- spotify-clone-back/src/main/java/com/amine/spotifycloneback/usercontext/mapper/UserMapper.java
+++ spotify-clone-back/src/main/java/com/amine/spotifycloneback/usercontext/mapper/UserMapper.java
@@ -1,8 +1,11 @@
 package com.amine.spotifycloneback.usercontext.mapper;
 
+import com.amine.spotifycloneback.usercontext.ReadUserDTO;
+import com.amine.spotifycloneback.usercontext.domain.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    ReadUserDTO readUserDTOToUser(User entity);
 }
