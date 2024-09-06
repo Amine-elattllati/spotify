f1c6f7d764c5053c81e65f6d679cfb5f01272299
AMINE ELATTLLATI <amine.elattllati@gmail.com>
2024-09-06 10:56:18 +0100

    Authentication configuration (Backend)

--- spotify-clone-back/src/main/java/com/amine/spotifycloneback/usercontext/repository/UserRepository.java
+++ spotify-clone-back/src/main/java/com/amine/spotifycloneback/usercontext/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.amine.spotifycloneback.usercontext.repository;
 import com.amine.spotifycloneback.usercontext.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneByEmail(String email);
 }
